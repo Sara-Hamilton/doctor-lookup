fb6c6c2eef0293960f79f03a059d33b6be474d8e
Sara Hamilton <sara@indeterminate.net>
2018-03-23 10:03:24 -0700

    add ability to get all required info for first doctor

--- src/js/api.js
+++ src/js/api.js
@@ -10,9 +10,19 @@ export function specialtySearch(condition) {
   },
   success: function(response) {
     console.log(response);
-    let reply = response.data[0].profile.first_name;
-    console.log(reply);
-    $('#condition-first-name').append(`<p>${reply}</p>`);
+    let firstName = response.data[0].profile.first_name;
+    let lastName = response.data[0].profile.last_name;
+    let address = response.data[0].practices[0].visit_address;
+    let phone = response.data[0].practices[0].phones;
+    let website = response.data[0].practices[0].website;
+    let newPatients = response.data[0].practices[0].accepts_new_patients;
+    console.log("first", firstName);
+    console.log("last", lastName);
+    console.log("address", address);
+    console.log("phone", phone);
+    console.log("web", website);
+    console.log("new patients", newPatients);
+    $('#condition-first-name').append(`<p>Name: ${firstName} ${lastName}</p>`);
   },
   error: function() {
     $('.error').text(`There was a problem.`)
