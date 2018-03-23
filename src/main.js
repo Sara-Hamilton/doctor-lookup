0224908947f6de37196319ad20e36e18e2dee139
Sara Hamilton <sara@indeterminate.net>
2018-03-23 14:08:28 -0700

    modify styling of error messages

--- src/main.js
+++ src/main.js
@@ -4,28 +4,29 @@ import { nameSearch } from './js/name.js';
 
 $(document).ready(function() {
   $("#condition-form").submit(function(event) {
-  event.preventDefault();
-  const condition = $("#condition").val();
-  const conditionLimit = $("#condition-limit").val();
-  $(".well").hide();
-  $('#reset-button').show();
-  conditionSearch(condition, conditionLimit);
+    event.preventDefault();
+    const condition = $("#condition").val();
+    const conditionLimit = $("#condition-limit").val();
+    $(".well").hide();
+    $('#reset-button').show();
+    conditionSearch(condition, conditionLimit);
   });
 
   $("#doctor-name-form").submit(function(event) {
-  event.preventDefault();
-  const doctorName = $("#doctor-name").val();
-  const nameLimit = $("#name-limit").val();
-  $(".well").hide();
-  $('#reset-button').show();
-  nameSearch(doctorName, nameLimit);
+    event.preventDefault();
+    const doctorName = $("#doctor-name").val();
+    const nameLimit = $("#name-limit").val();
+    $(".well").hide();
+    $('#reset-button').show();
+    nameSearch(doctorName, nameLimit);
   });
 
   $('#reset-button').click(function(event) {
-    event.preventDefault();
+    // event.preventDefault();
     $('#condition-form')[0].reset();
     $('#doctor-name-form')[0].reset();
     $('#results').empty();
+    $('.error').empty();
     $('.well').show();
     $('#reset-button').hide();
   });
