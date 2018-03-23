a2c73cdcfbb2f2b09eaab02c57ded951226157c8
Sara Hamilton <sara@indeterminate.net>
2018-03-23 11:34:25 -0700

    change new patiens response from true/false to yes/no

--- src/js/api.js
+++ src/js/api.js
@@ -1,47 +1,49 @@
 export function specialtySearch(condition, limit) {
   $.ajax({
-  url:
-  `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&skip=0&limit=${limit}&user_key=${process.env.exports.apiKey}`,
-  type: 'GET',
-  data: {
-    format: 'json'
-  },
-  success: function(response) {
-    if (response.data.length === 0) {
-      $('.error').text('The search provided no matching results.')
-    }
-    console.log(response);
-    for (let i = 0; i < response.data.length; i++) {
-      let firstName = response.data[i].profile.first_name;
-      let lastName = response.data[i].profile.last_name;
-      let address = response.data[i].practices[0].visit_address;
-      let phone = response.data[i].practices[0].phones;
-      let website = response.data[i].practices[0].website;
-      if (website === undefined) {
-        website = "unavailable";
+    url:
+    `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&skip=0&limit=${limit}&user_key=${process.env.exports.apiKey}`,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: function(response) {
+      if (response.data.length === 0) {
+        $('.error').text('The search provided no matching results.')
       }
-      let newPatients = response.data[i].practices[0].accepts_new_patients;
-      console.log("first", firstName);
-      console.log("last", lastName);
-      console.log("address", address);
-      console.log("phone", phone);
-      console.log("web", website);
-      console.log("new patients", newPatients);
-      $('#condition-first-name').append(`<div class="panel panel-default">
-                                          <div class="panel-heading"><h4>Name: ${firstName} ${lastName}</h4></div>
-                                          <div class="panel-body">
-                                          <p>Address: ${address.street}, ${address.city}, ${address.state} ${address.zip}</p>
-                                          <p>Phone: ${phone[0].number} Type: ${phone[0].type}</p>
-                                          <p>Website: ${website}</p>
-                                          <p>Accepting new patients: ${newPatients}</p></div></div>`);
-    }
+      console.log(response);
+      for (let i = 0; i < response.data.length; i++) {
+        let firstName = response.data[i].profile.first_name;
+        let lastName = response.data[i].profile.last_name;
+        let address = response.data[i].practices[0].visit_address;
+        let phone = response.data[i].practices[0].phones;
+        let website = response.data[i].practices[0].website;
+        if (website === undefined) {
+          website = "unavailable";
+        }
+        let newPatients = response.data[i].practices[0].accepts_new_patients;
+        if (newPatients === true) {
+          newPatients = "yes";
+        } else {
+          newPatients = "no";
+        }
+        console.log("first", firstName);
+        console.log("last", lastName);
+        console.log("address", address);
+        console.log("phone", phone);
+        console.log("web", website);
+        console.log("new patients", newPatients);
+        $('#condition-first-name').append(`<div class="panel panel-default">
+                                            <div class="panel-heading"><h4>Name: ${firstName} ${lastName}</h4></div>
+                                            <div class="panel-body">
+                                            <p>Address: ${address.street}, ${address.city}, ${address.state} ${address.zip}</p>
+                                            <p>Phone: ${phone[0].number} Type: ${phone[0].type}</p>
+                                            <p>Website: ${website}</p>
+                                            <p>Accepting new patients: ${newPatients}</p></div></div>`);
+      }
 
-  },
-  error: function() {
-    if (response.data.length === 0) {
-      $('.error').text('The search provided no matching results.')
+    },
+    error: function() {
+      $('.error').text(`There was an error handling your request.`)
     }
-    $('.error').text(`There was a problem.`)
-  }
-});
+  });
 }
